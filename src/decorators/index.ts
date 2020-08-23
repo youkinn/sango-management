6b80f9d8b9d1bc188e351f845cb0433d0ce3dc99
youkinn <03338277@163.com>
2020-08-23 23:40:14 +0800

    Update：修改防抖/节流函数允许传入自定义时间

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -2,25 +2,22 @@
  * @Description: 自定义装饰器
  * @Autor: 胡椒
  * @Date: 2020-08-21 12:47:10
- * @LastEditors: 胡椒
- * @LastEditTime: 2020-08-22 18:05:11
+ * @LastEditors: youkinn
+ * @LastEditTime: 2020-08-23 23:36:10
  */
+import { debounce as debounceFn, throttle as throttleFn } from 'lodash';
 import ExtendedVue from 'vue';
 
 /** 防抖 */
-export const debounce = (
-  target: ExtendedVue,
-  propertyKey: string,
-  descriptor: PropertyDescriptor
-): void => {
-  descriptor.value = target.$util.debounce(descriptor.value, 3000);
+export const debounce = (wait?: number): Function => {
+  return (target: ExtendedVue, propertyKey: string, descriptor: PropertyDescriptor) => {
+    descriptor.value = debounceFn(descriptor.value, wait || 3000);
+  };
 };
 
 /** 节流 */
-export const throttle = (
-  target: ExtendedVue,
-  propertyKey: string,
-  descriptor: PropertyDescriptor
-): void => {
-  descriptor.value = target.$util?.throttle(descriptor.value, 3000);
+export const throttle = (wait?: number): Function => {
+  return (target: ExtendedVue, propertyKey: string, descriptor: PropertyDescriptor) => {
+    descriptor.value = throttleFn(descriptor.value, wait || 3000);
+  };
 };
